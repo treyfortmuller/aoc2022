e09d0df3ea9764b70d2b086cb4daae489690d0b1
Trey Fortmuller <tfortmuller@anduril.com>
2022-12-03 04:09:15 -0600

    part2 for day3, this is brutal

--- day3/day3.nix
+++ day3/day3.nix
@@ -2,46 +2,62 @@ let
   pkgs = import <nixpkgs> { };
   lib = pkgs.lib;
 
-  inherit (builtins) readFile substring stringLength map elemAt filter length;
+  inherit (builtins)
+    readFile substring stringLength map elem elemAt filter length mapAttrs head
+    genList;
 
   listSum = lib.foldr (l: r: l + r) 0;
 
-  alphabetSoup = lib.stringToCharacters
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  indices = builtins.genList (x: x + 1) (length alphabetSoup);
-  priorityLookup = lib.zipLists indices alphabetSoup;
-  getPriority = l: (builtins.head (filter (x: x.snd == l) priorityLookup)).fst;
+  getPriority = l:
+    let
+      alphabetSoup = lib.stringToCharacters
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      indices = genList (x: x + 1) (length alphabetSoup);
+      priorityLookup = lib.zipLists indices alphabetSoup;
+    in (head (filter (x: x.snd == l) priorityLookup)).fst;
 
   # input = readFile ./example.txt;
   input = readFile ./input.txt;
-
   rucksacks = lib.splitString "\n" input;
-
-  # Helper function to return the length of a string divded by two
-  halfLen = s: stringLength s / 2;
 
   # Given a string representation of the contents of one rucksack, split
   # the string into halves in an attrset
-  compartmentSplit = c: {
-    first = lib.stringToCharacters (substring 0 (halfLen c) c);
-    second = lib.stringToCharacters (substring (halfLen c) (stringLength c) c);
-  };
-
-  compartmentContentLists = map compartmentSplit rucksacks;
-
-  sharedContents = map (x: builtins.head (lib.intersectLists x.first x.second))
-    compartmentContentLists;
+  compartmentSplit = c:
+    let halfLen = s: stringLength s / 2;
+    in {
+      first = lib.stringToCharacters (substring 0 (halfLen c) c);
+      second =
+        lib.stringToCharacters (substring (halfLen c) (stringLength c) c);
+    };
 
   # Part 2
-  chunksOf = n: l:
+  compartmentContentLists = map compartmentSplit rucksacks;
+
+  sharedContents =
+    map (x: head (lib.intersectLists x.first x.second)) compartmentContentLists;
+
+  tripleAttr = l: {
+    first = elemAt l 0;
+    second = elemAt l 1;
+    third = elemAt l 2;
+  };
+
+  chunksOfThree = l:
     if (length l == 0) then
       [ ]
     else
-      lib.singleton (lib.take n l) ++ chunksOf n (lib.drop n l);
+      lib.singleton (tripleAttr (lib.take 3 l)) ++ chunksOfThree (lib.drop 3 l);
 
+  # Given a three-element attrset of strings representing the contents of a group of rucksack, 
+  # find the shared element of all 3 sacks.
+  findShared = attrset:
+    let
+      chared = stringsToChars attrset;
+      elemMulti = x: l1: l2: elem x l1 && elem x l2;
+      stringsToChars = l: mapAttrs (_: v: lib.stringToCharacters v) l;
+    in lib.findFirst (e: elemMulti e chared.second chared.third)
+    (throw "no shared contents") chared.first;
 in {
   part1 = listSum (map getPriority sharedContents);
-
-  # TODO: complete part2 from here
-  part2 = chunksOf 3 [ 1 2 3 4 5 6 ];
+  part2 = listSum (map getPriority (map findShared (chunksOfThree rucksacks)));
 }
